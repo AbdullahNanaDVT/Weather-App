eed292e750ca9cc42a4724d80150cb4d0ed422c7
Abdullah Nana <anana@jhb.dvt.co.za>
2021-08-27 15:27:42 +0200

    Initial commit

---
+++ Weather App/Weather App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather App
+//
+//  Created by Abdullah Nana on 2021/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
