387580c2ba1e4957f54c71754b5a2eb87175781d
Abdullah Nana <anana@jhb.dvt.co.za>
2021-08-29 14:31:37 +0200

    Added SwiftLint

--- Weather App/Weather AppTests/Weather_AppTests.swift
+++ Weather App/Weather AppTests/Weather_AppTests.swift
@@ -8,7 +8,7 @@
 import XCTest
 @testable import Weather_App
 
-class Weather_AppTests: XCTestCase {
+class WeatherAppTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
